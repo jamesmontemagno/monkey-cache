736dc6ed089ca56d2ddf5acfb657699a7abc628d
James Montemagno <james.montemagno@gmail.com>
2017-12-13 00:50:26 -0800

    Add sample apps
    iOS/Android/UWP/ASPNetCore

---
+++ src/MonkeyCache.TestApp.AspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MonkeyCache.TestApp.AspNetCore.Models;
+using Ooui;
+using Ooui.AspNetCore;
+using Ooui.Forms;
+using Xamarin.Forms;
+
+namespace MonkeyCache.TestApp.AspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var element = new MainPage().GetOouiElement();
+            return new ElementResult(element, title: "Monkey Cache!");
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/MonkeyCache.TestApp/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MonkeyCache.TestApp
+{
+	public partial class MainPage : ContentPage
+	{
+        class Monkey
+        {
+            public string Name { get; set; }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            ButtonLoad.Clicked += ButtonLoad_Clicked;
+            ButtonSave.Clicked += ButtonSave_Clicked;
+
+            var monkey = Barrel.Current.Get<Monkey>("monkey");
+            if (monkey != null)
+                EntryName.Text = monkey.Name;
+            else
+                EntryName.Text = "Sebastian";
+
+            ButtonExpired.Clicked += ButtonExpired_Clicked;
+
+        }
+
+        private void ButtonExpired_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Is Expired?", Barrel.Current.IsExpired("monkey") ? "Yes" : "No", "OK");
+        }
+
+        private void ButtonSave_Clicked(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(EntryName.Text))
+            {
+                DisplayAlert("Info", "Please enter a name", "OK");
+                return;
+            }
+            var monkey = new Monkey { Name =  EntryName.Text};
+            Barrel.Current.Add<Monkey>("monkey", monkey, TimeSpan.FromDays(1));
+            DisplayAlert(":)", "Saved!", "OK");
+        }
+
+        private void ButtonLoad_Clicked(object sender, EventArgs e)
+        {
+            var monkey = Barrel.Current.Get<Monkey>("monkey");
+            if (monkey == null)
+                DisplayAlert(":(", "No Monkey", "OK");
+            else
+                DisplayAlert(":)", monkey.Name, "OK");
+        }
+    }
+}
