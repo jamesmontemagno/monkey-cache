de5f777352db114b7bf5780c6565602ec746504c
James Montemagno <james.montemagno@gmail.com>
2017-12-19 15:25:48 -0800

    Update Tests

---
+++ src/MonkeyCache.TestsFileStore/BarrelTests.cs
@@ -0,0 +1,16 @@
+﻿using MonkeyCache.FileStore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeyCache.Tests
+{
+    public partial class BarrelTests
+    {
+		void SetupBarrel()
+		{
+			Barrel.ApplicationId = "com.refractored.monkeycacheldb";
+			barrel = Barrel.Current;
+		}
+	}
+}
