a193a157186b283805602aa3a6699690374aab36
Michael Stonis <michael.stonis@eightbot.com>
2017-12-21 07:25:17 -0600

    Starting implementation of Realm implementation

---
+++ src/MonkeyCache.Realm/Barrel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Realms;
+
+namespace MonkeyCache.Realm
+{
+    public class Barrel : IBarrel
+    {
+        public void Add(string key, string data, TimeSpan expireIn, string eTag = null){
+            
+        }
+
+        public void Add<T>(string key, T data, TimeSpan expireIn, string eTag = null){
+            
+        }
+
+        public void Empty(params string[] key){
+
+        }
+
+        public void EmptyAll(){
+
+        }
+
+        public void EmptyExpired(){
+
+        }
+
+        public bool Exists(string key){
+
+        }
+
+        public string Get(string key){
+
+        }
+
+        public T Get<T>(string key){
+
+        }
+
+        public string GetETag(string key){
+
+        }
+
+        public bool IsExpired(string key){
+
+        }
+    }
+}
