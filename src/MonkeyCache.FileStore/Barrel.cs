9042d0ea3fbf66e991e307412b0a55a8a5ffa9ab
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-12-21 18:47:20 -0500

    Added file store implementation

--- src/MonkeyCache.FileStore/Barrel.cs
+++ src/MonkeyCache.FileStore/Barrel.cs
@@ -226,7 +226,26 @@ namespace MonkeyCache.FileStore
 
 			try
 			{
-				return index.Keys;
+				if (index != null)
+				{
+					var bananas = new List<KeyValuePair<string, Tuple<string, DateTime>>>();
+
+					if (state.HasFlag(CacheState.Active))
+					{
+						bananas = index
+							.Where(x => x.Value.Item2 >= DateTime.UtcNow)
+							.ToList();
+					}
+
+					if (state.HasFlag(CacheState.Expired))
+					{
+						bananas.AddRange(index.Where(x => x.Value.Item2 < DateTime.UtcNow));
+					}
+
+					return bananas.Select(x => x.Key);
+				}
+
+				return new string[0];
 			}
 			catch (Exception)
 			{
